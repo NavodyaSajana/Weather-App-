25b7a92c61c750224590f3a346e4ea259baaf920
IshanKaushalya <navodyasajana234@gmail.com>
2024-04-04 12:17:12 +0530

    sajana/connection_to _database

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const sql = require('mssql');
+
 const app = express();
+
+const config = {
+  user: 'sjnadmin',
+  password: 'Sjn@admin123',
+  server: 'sjnweathersrilanka.database.windows.net',
+  database: 'Weather_db',
+  options: {
+    encrypt: true
+  }
+};
+
 const port = process.env.PORT || 3000;
+
+sql.connect(config)
+.then(pool => {
+  console.log('Connected to Azure SQL DB');
+})
+.catch(err => {
+  console.error('Database connection failed: ',err);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
