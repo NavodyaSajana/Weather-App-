2739c0483cd619ca2d14497491f60b4814db77e5
IshanKaushalya <navodyasajana234@gmail.com>
2024-04-05 18:10:05 +0530

    sajana/enable_cors_mapping

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const sql = require('mssql');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 const config = {
   user: 'sjnadmin',
